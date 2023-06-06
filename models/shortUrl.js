eba27e79cee78c22daccd4d34e054520f671e56d
shemamoses <mosesshema1@gmail.com>
2023-06-07 01:24:43 +0200

    .

---
+++ models/shortUrl.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const shortId = require("shortid");
+
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate,
+  },
+});
+
+module.exports = mongoose.model("ShortUrl", shortUrlSchema);
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const shortUrl = require("./models/shortUrl");
 
 const DB = process.env.DATABASE_LOCAL || "mongodb://localhost/url-shortener";
 
 mongoose.connect(DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  
 });
 
 const app = express();
 const port = process.env.PORT;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -22,4 +25,7 @@ app.listen(port || 5000, () => {
   console.log("server running successfully");
 });
 
-app.post("/shorten", (req, res) => {});
+app.post("/shorten", async(req, res) => {
+  await shortUrl.create({ full: req.body.url });
+  res.redirect("/");
+});
