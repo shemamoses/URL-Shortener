c81cefe50fcfba43ce114f60852b6111b9f118d2
shemamoses <mosesshema1@gmail.com>
2023-06-07 00:02:41 +0200

    render html

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT;
 
+app.set("view engine", "ejs");
 
-app.listen(port, () => {
-  console.log('server running successfully');
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port || 5000, () => {
+  console.log("server running successfully");
 });
