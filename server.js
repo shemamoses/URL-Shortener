b836dcab8f9189976ad60e13b4fe08828506562b
shemamoses <mosesshema1@gmail.com>
2023-06-06 23:38:46 +0200

    running the server

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-const express = require('express')
+const express = require("express");
+const dotenv = require("dotenv");
 
+const app = express();
+const port = process.env.PORT;
+
+
+app.listen(port, () => {
+  console.log('server running successfully');
+});
